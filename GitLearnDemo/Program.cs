a385c05f52b04a41ccca51bae717d11e4927373e
Uttam Kumar <uttam@EVISION.COM>
2026-02-25 10:53:48 +0530

    Added gitignore file

---
+++ GitLearnDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("=== HRMS Console Demo ===");
+
+        //Console.Write("Enter Employee Name: ");
+        //string empName = Console.ReadLine();
+
+        //Console.Write("Enter Salary: ");
+        //double salary = Convert.ToDouble(Console.ReadLine());
+
+        //Console.WriteLine("\nEmployee Details:");
+        //Console.WriteLine("Name: " + empName);
+        //Console.WriteLine("Salary: " + salary);
+
+        //Console.WriteLine("\nProcess Completed Successfully!");
+        //Console.ReadLine();
+    }
+}
