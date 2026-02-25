a622d2718a7290390566d9f707c51a9666c46e62
Devanshiben Suthar <devanshi@evisionits.com>
2026-02-25 11:31:04 +0530

    adding employee information

--- GitLearnDemo/Program.cs
+++ GitLearnDemo/Program.cs
@@ -6,17 +6,18 @@ class Program
     {
         Console.WriteLine("=== HRMS Console Demo ===");
 
-        //Console.Write("Enter Employee Name: ");
-        //string empName = Console.ReadLine();
+        Console.Write("Enter Employee Name: ");
+        string empName = Console.ReadLine();
 
-        //Console.Write("Enter Salary: ");
-        //double salary = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter Salary: ");
+        double salary = Convert.ToDouble(Console.ReadLine());
 
-        //Console.WriteLine("\nEmployee Details:");
-        //Console.WriteLine("Name: " + empName);
-        //Console.WriteLine("Salary: " + salary);
+        Console.Write("Enter Salary: ");
+        Console.WriteLine("\nEmployee Details:");
+        Console.WriteLine("Name: " + empName);
+        Console.WriteLine("Salary: " + salary);
 
-        //Console.WriteLine("\nProcess Completed Successfully!");
-        //Console.ReadLine();
+        Console.WriteLine("\nProcess Completed Successfully!");
+        Console.ReadLine();
     }
 }
